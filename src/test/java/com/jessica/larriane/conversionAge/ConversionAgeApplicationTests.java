9a8e04f21aea02020d22d7bf36d7760da2448026
Jessica Larriane <larrianejessica@gmail.com>
2020-06-24 19:41:00 -0300

    first commit

---
+++ src/test/java/com/jessica/larriane/conversionAge/ConversionAgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessica.larriane.conversionAge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionAgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
